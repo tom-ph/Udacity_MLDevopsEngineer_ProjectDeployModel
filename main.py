c698760f85a409d049faefb7664874c877a355b8
tommaso <tommaso.fou@gmail.com>
2022-03-04 23:05:58 +0000

    added dvc hardlink lock true configuration

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import os
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
+    os.system("dvc config core.hardlink_lock true")
     if os.system("dvc pull") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
