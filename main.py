7aa5bd6cc1c289e7d31eebfd8561a7767ffd8af3
tommaso <tommaso.fou@gmail.com>
2022-03-04 23:20:37 +0000

    added force to dvc pull

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import os
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
     os.system("dvc config core.hardlink_lock true")
-    if os.system("dvc pull") != 0:
+    if os.system("dvc pull --force") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
