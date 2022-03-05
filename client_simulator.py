e661af14b0c6eedba7aef01e41dc6a80a7f45ddb
tommaso <tommaso.fou@gmail.com>
2022-03-05 03:36:32 +0000

    tested client simulator

--- client_simulator.py
+++ client_simulator.py
@@ -7,5 +7,10 @@ sys.path.append(os.getcwd())
 # Import our app from main.py.
 from main import InputData
 
+BASE_URL = "https://udacitymlops-project-deploy.herokuapp.com/"
+
 data = InputData.Config.schema_extra["example"]
-r = requests.post("http://localhost:8000/predict/", json=data)
+r = requests.post(BASE_URL + "predict/", json=data)
+print(r.status_code)
+print(r.json())
+1==1
