54f81120ebeea23ba8aade08e3d205e0e27ecf7d
tommaso <tommaso.fou@gmail.com>
2022-03-05 01:42:02 +0000

    restored removing of .dvc folder

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if "DYNO" in os.environ and os.path.isdir(".dvc"):
         print("dvc pull may have failed failed with status code " + str(pull_result))
         os.system("rm -r .dvc/tmp/lock")
         pull_result = os.system("dvc pull --force")
-    #os.system("rm -r .dvc .apt/usr/lib/dvc")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 app = FastAPI()
 
